ef70e84600d7cb4c4994939935b9e34f0d6d763b
Devalah <wallerskai@gmail.com>
2025-05-06 20:41:31 -0600

    sfen

--- public/functions/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require('firebase-functions');
-const express = require('express');
-const app = express();
-
-app.get('/api', (req, res) => {
-  res.send('Hello from Firebase!');
-});
-
-exports.api = functions.https.onRequest(app);
