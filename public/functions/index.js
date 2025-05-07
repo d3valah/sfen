9efde31fbb72cd47cad9c51acb5e295b810223c6
Devalah <wallerskai@gmail.com>
2025-05-06 20:03:41 -0600

    pp

--- public/functions/index.js
+++ public/functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 
-app.get('/api/hello', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hello from Firebase!');
 });
 
